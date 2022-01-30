15fd145340d9325e14289ec89891f50a123c641b
Nicole Candiotti <nccandiotti@gmail.com>
2022-01-30 10:59:06 -0500

    commit tree ebb8c7ef99b5a8744be2e76d8a5349c78512f26c

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,12 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import React from "react";
+import "@testing-library/jest-dom"
+import { render, screen } from "@testing-library/react"
+import React from "react"
 
-import Article from "../components/Article";
+import Article from "../components/Article"
 
 test("displays the text 'please pass this test'", () => {
-  render(<Article />);
+  render(<Article />)
+  screen.debug()
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
-});
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument()
+})
